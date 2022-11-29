fcc1a5c6f113887c85c44d048b9885d58bd6c2d2
Jerry <gch02933@naver.com>
2022-11-29 13:54:40 +0900

    feat: js 생성 및 submenu 기능 추가

---
+++ public/js/index.js
@@ -0,0 +1,99 @@
+const gnb_list1 = document.querySelector('#gnb_list1')
+const gnb_list2 = document.querySelector('#gnb_list2')
+const gnb_list3 = document.querySelector('#gnb_list3')
+const gnb_list4 = document.querySelector('#gnb_list4')
+const gnb_list5 = document.querySelector('#gnb_list5')
+const gnb_list6 = document.querySelector('#gnb_list6')
+const gnb_list7 = document.querySelector('#gnb_list7')
+const gnb_list8 = document.querySelector('#gnb_list8')
+const gnb_list9 = document.querySelector('#gnb_list9')
+const gnb_list10 = document.querySelector('#gnb_list10')
+const gnb_list11 = document.querySelector('#gnb_list11')
+
+gnb_list1.addEventListener('mouseover', function(){
+    gnb_list1.classList.add('on')
+})
+
+gnb_list1.addEventListener('mouseout', function(){
+    gnb_list1.classList.remove('on')
+})
+
+gnb_list2.addEventListener('mouseover', function(){
+    gnb_list2.classList.add('on')
+})
+
+gnb_list2.addEventListener('mouseout', function(){
+    gnb_list2.classList.remove('on')
+})
+
+gnb_list3.addEventListener('mouseover', function(){
+    gnb_list3.classList.add('on')
+})
+
+gnb_list3.addEventListener('mouseout', function(){
+    gnb_list3.classList.remove('on')
+})
+
+gnb_list4.addEventListener('mouseover', function(){
+    gnb_list4.classList.add('on')
+})
+
+gnb_list4.addEventListener('mouseout', function(){
+    gnb_list4.classList.remove('on')
+})
+
+gnb_list5.addEventListener('mouseover', function(){
+    gnb_list5.classList.add('on')
+})
+
+gnb_list5.addEventListener('mouseout', function(){
+    gnb_list5.classList.remove('on')
+})
+
+gnb_list6.addEventListener('mouseover', function(){
+    gnb_list6.classList.add('on')
+})
+
+gnb_list6.addEventListener('mouseout', function(){
+    gnb_list6.classList.remove('on')
+})
+
+gnb_list7.addEventListener('mouseover', function(){
+    gnb_list7.classList.add('on')
+})
+
+gnb_list7.addEventListener('mouseout', function(){
+    gnb_list7.classList.remove('on')
+})
+
+gnb_list8.addEventListener('mouseover', function(){
+    gnb_list8.classList.add('on')
+})
+
+gnb_list8.addEventListener('mouseout', function(){
+    gnb_list8.classList.remove('on')
+})
+
+gnb_list9.addEventListener('mouseover', function(){
+    gnb_list9.classList.add('on')
+})
+
+gnb_list9.addEventListener('mouseout', function(){
+    gnb_list9.classList.remove('on')
+})
+
+gnb_list10.addEventListener('mouseover', function(){
+    gnb_list10.classList.add('on')
+})
+
+gnb_list10.addEventListener('mouseout', function(){
+    gnb_list10.classList.remove('on')
+})
+
+gnb_list11.addEventListener('mouseover', function(){
+    gnb_list11.classList.add('on')
+})
+
+gnb_list11.addEventListener('mouseout', function(){
+    gnb_list11.classList.remove('on')
+})
